82a754655349c5f8b6dd5400a65a4009a41a8c66
tonysingh113 <tonysingh112000@gmail.com>
2023-09-01 10:39:50 +0530

    Update settings.py

--- ClinicalProject/settings.py
+++ ClinicalProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-f-qiw8#52w_ze+*z#1nb7uq^yt#+t9-c6eu!^(he^(--7#0x*u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-13-53-200-39.eu-north-1.compute.amazonaws.com']
 
 
 # Application definition
