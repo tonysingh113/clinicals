7005fe877e78b4e5d7284ac786aa66717aa0f73e
tonysingh113 <tonysingh112000@gmail.com>
2023-09-01 10:46:07 +0530

    Update models.py

--- ClinicalApp/models.py
+++ ClinicalApp/models.py
@@ -6,7 +6,7 @@ class Patient(models.Model):
     lastname=models.CharField(max_length=20)
     firstname=models.CharField(max_length=20)
     age=models.IntegerField()
-
+#hello
 class ClinicalData(models.Model):
     COMPONENT_NAMES=[('hw','Height/Weight'),('bp','Blood Pressure'),('heartrate','Heart Rate')]
     componentname=models.CharField(choices=COMPONENT_NAMES,max_length=50)
